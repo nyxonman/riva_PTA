88a77a8f4fbaf20578279352b9df54ebef6a55d1
Shakya <nishakya@itron.com>
2019-02-13 14:58:48 +0100

    centered the UI

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ if __name__ == "__main__":
     import sys
     app     = QtWidgets.QApplication(sys.argv)
     MainObj = QtWidgets.QMainWindow()
-    
+    centerPoint     = QDesktopWidget().availableGeometry().center()
+
     # initialize the ui
     ui = Ui_MainObj()
     ui.setupUi(MainObj)
     MainObj.show()
+    centerPoint = centerPoint - QtCore.QPoint(MainObj.width()/2,MainObj.height()/2)
+    MainObj.move(centerPoint)
     
     # rename the window
     MainObj.setWindowTitle("Ping Test Application v" + APP_VERSION)
